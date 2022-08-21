419c62b8b4f81054e360d7d4f10886cad36a8017
BayuDC <dev.bayudc@gmail.com>
2022-08-21 10:45:38 +0700

    chore: update database seeder

--- api/prisma/seed.js
+++ api/prisma/seed.js
@@ -2,21 +2,23 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-    await prisma.class.create({
-        data: {
-            name: 'Kelas Uji Coba',
-            students: {
-                createMany: {
-                    data: [...Array(5)].map((_, i) => ({
-                        no: 11 + i,
-                        fullname: 'Student' + i,
-                        username: 'student' + i,
-                        password: 'student' + i,
-                    })),
+    for (const [i, className] of ['XI TKJ 1', 'XI TKJ 2', 'XI TKJ 3', 'XI TKJ 4'].entries()) {
+        await prisma.class.create({
+            data: {
+                name: className,
+                students: {
+                    createMany: {
+                        data: [...Array(10)].map((_, j) => ({
+                            no: j + 1,
+                            username: `${j + 1}@tkj${i + 1}`,
+                            fullname: 'Student ' + (j + 1),
+                            password: `${j + 1}`,
+                        })),
+                    },
                 },
             },
-        },
-    });
+        });
+    }
 }
 
 main()
