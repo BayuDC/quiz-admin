91f15baeb88091c1b3febde3d45bd34ad73efd3c
BayuDC <dev.bayudc@gmail.com>
2022-08-21 08:04:32 +0700

    chore: define the `/questions`

--- api/routes/question.js
+++ api/routes/question.js
@@ -1,4 +1,5 @@
 const schema = require('../schemas/question');
+const handler = require('../handlers/question');
 
 /**
  * @param {import('fastify').FastifyInstance} fastify
@@ -6,13 +7,26 @@ const schema = require('../schemas/question');
  * @param {import('fastify').FastifyPluginCallback} done
  */
 module.exports = function (fastify, options, done) {
+    fastify.get('/questions', {
+        handler: handler.index,
+    });
+    fastify.get('/questions/:questionid', {
+        preHandler: handler.load,
+        handler: handler.show,
+    });
     fastify.post('/questions', {
-        preHandler: [fastify.validateAsync(schema)],
-        handler: () => {
-            return { ok: 1 };
-        },
+        preHandler: fastify.validateAsync(schema),
+        handler: handler.store,
         schema,
     });
-
+    fastify.put('/questions/:questionid', {
+        preHandler: [handler.load, fastify.validateAsync(schema)],
+        handler: handler.update,
+        schema,
+    });
+    fastify.delete('/questions/:questionId', {
+        preHandler: handler.load,
+        handler: handler.destroy,
+    });
     done();
 };
